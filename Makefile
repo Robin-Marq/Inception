e5e89766e98a61ebf5a4a265b974149eb663a09b
Robin Marquardt <129085102+Robin-Marq@users.noreply.github.com>
2024-05-21 14:09:29 +0200

    #update mariaDB

--- Makefile
+++ Makefile
@@ -6,7 +6,20 @@
 #    By: rmarquar <rmarquar@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/05/13 16:12:34 by rmarquar          #+#    #+#              #
-#    Updated: 2024/05/13 16:12:35 by rmarquar         ###   ########.fr        #
+#    Updated: 2024/05/21 10:22:48 by rmarquar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+all: up
+
+up:
+	docker compose -f ./srcs/docker-compose.yml up -d
+
+down:
+	docker compose -f ./srcs/docker-compose.yml down
+
+start:
+	docker compose -f ./srcs/docker-compose.yml start
+
+stop:
+	docker compose -f ./srcs/docker-compose.yml stop
--- srcs/requirements/mariadb/conf/init.sql
+++ srcs/requirements/mariadb/conf/init.sql
@@ -1,15 +1,3 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    init.sql                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: rmarquar <rmarquar@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/05/21 09:45:29 by rmarquar          #+#    #+#              #
-#    Updated: 2024/05/21 09:47:22 by rmarquar         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 USE mysql;
 CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'secret';
